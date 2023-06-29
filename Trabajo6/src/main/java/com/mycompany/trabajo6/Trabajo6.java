8b94606e44e850964a539ef7f4631fb6941e17f6
JuanEstebanCC <juanescifuentes75@gmail.com>
2023-06-29 11:09:27 -0500

    Create branch and upload Trabajo 6

---
+++ Trabajo6/src/main/java/com/mycompany/trabajo6/Trabajo6.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajo6;
+
+/**
+ *
+ * @author santi
+ */
+public class Trabajo6 {
+
+    public static void main(String[] args) {
+        Window ventana= new Window();
+        ventana.setVisible(true);
+    }
+
+}
